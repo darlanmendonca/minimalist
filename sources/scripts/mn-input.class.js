bbbcafbac9d8d1048e49af4364da0b93224e76dd
Darlan Mendonça <darlanmendonca@gmail.com>
2017-04-25 01:06:25 -0300

    debug the window.customElements from jsdom

--- sources/scripts/mn-input.class.js
+++ sources/scripts/mn-input.class.js
@@ -4,4 +4,10 @@ class MnInput {
   }
 }
 
+// window.customElements.define('mn-input', 'MnInput')
+// window.MnInput = MnInput
+
+// https://github.com/tmpvar/jsdom/issues/1030
+// console.log(window.customElements)
+
 module.exports = MnInput
--- sources/scripts/mn-input.spec.js
+++ sources/scripts/mn-input.spec.js
@@ -1,15 +1,13 @@
 const {describe, it, before} = require('mocha')
 const {expect} = require('chai')
 
-let MnInput
-
 before(() => {
-  MnInput = require('./mn-input.class.js')
+  require('./mn-input.class.js')
 })
 
 describe('lorem ipsum', () => {
   it('a simple unit test', () => {
-    const input = new MnInput()
-    expect(input).to.have.property('test')
+    expect(true).to.be.true
+    // expect(window).to.have.property('MnInput')
   })
 })
