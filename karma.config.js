514f30c1f320d0953a7e9a5fedc7cde5028ea60f
Darlan Mendonça <darlanmendonca@gmail.com>
2017-05-12 09:33:29 -0300

    enable browserstack in CI

--- karma.config.js
+++ karma.config.js
@@ -13,7 +13,34 @@ function KarmaConfig(config) {
       'Safari',
       'Firefox',
       'Nightmare',
+      'browserstack:chrome',
+      'browserstack:safari',
+      'browserstack:firefox',
     ],
+
+    customLaunchers: {
+      'browserstack:chrome': {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: '57', // eslint-disable-line camelcase
+        os: 'Windows',
+        os_version: '10', // eslint-disable-line camelcase
+      },
+      'browserstack:firefox': {
+        base: 'BrowserStack',
+        browser: 'firefox',
+        browser_version: '53', // eslint-disable-line camelcase
+        os: 'Windows',
+        os_version: '10', // eslint-disable-line camelcase
+      },
+      'browserstack:safari': {
+        base: 'BrowserStack',
+        browser: 'safari',
+        browser_version: '10', // eslint-disable-line camelcase
+        os: 'OS X',
+        os_version: 'Sierra', // eslint-disable-line camelcase
+      },
+    },
 
     frameworks: [
       'browserify',
