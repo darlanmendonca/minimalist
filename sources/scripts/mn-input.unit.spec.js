cd7f65a3e2de3dc92330b6586015b7e160a12145
Darlan Mendonça <darlanmendonca@gmail.com>
2017-05-12 14:27:09 -0300

    switch customElements polyfill to @webcomponents/custom-elements

--- sources/scripts/mn-input.unit.spec.js
+++ sources/scripts/mn-input.unit.spec.js
@@ -15,19 +15,7 @@ function fallbackCustomElements() {
   const supportsCustomElements = 'customElements' in window
 
   if (!supportsCustomElements) {
-    // fallback to custom elements, using document-register-element
-    require('document-register-element/pony')(window)
-
-    // document-register-element dont call method connectedCallback
-    // maybe add this in document-register-element in a Pull Request be good
-    const {Element} = window
-    const appendChild = Element.prototype.appendChild
-    Element.prototype.appendChild = function(element){
-      appendChild.apply(this, arguments)
-      if (element && element.connectedCallback) {
-        element.connectedCallback()
-      }
-    }
+    require('@webcomponents/custom-elements')
   }
 }
 
