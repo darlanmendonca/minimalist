3ab5dea9c4392926f981c930f990cf4524f0bc8a
Darlan Mendonça <darlanmendonca@gmail.com>
2017-06-06 14:21:42 -0300

    polyfill to input date

--- docs/vendor.js
+++ docs/vendor.js
@@ -424,15 +424,72 @@ module.exports = class MnDate extends MnInput {
   _setInput() {
     super._setInput()
     this.input.setAttribute('type', 'date')
+    const supportsInputDate = this.input.type === 'date'
+
+    if (supportsInputDate) { // temp
+      this.input.setAttribute('type', 'text') // temp
+      this.input.setAttribute('maxlength', 10)
+      this._setMask()
+    }
+  }
+
+  _setMask() {
+    this.input.addEventListener('input', () => {
+      this.updateMask()
+    })
+
+    // this.input.addEventListener('change', () => {
+      // console.log(this.input.value)
+      // this.updateMask()
+    // })
+  }
+
+  updateMask() {
+    // const value = this.input.value
+    // const reg = /(\d{1,2})(?:\/)?(\d{1,2})?(?:\/)?(\d{1,})?/
+    // const pattern = /^\d{1,2}\/\d{1,2}\/\d{1,}/
+    // const match = value.match(reg)
+    // const day = match && match[1]
+    // const month = match && match[2]
+    // const year = match && match[3]
+
+    // !value.match(pattern)
+    //   ? this.input.value = [day, month, year].filter(item => Boolean(item)).join('/')
+    //   : null
   }
 
   get value() {
-    return this.input.value
-      ? new Date(this.input.value).toISOString()
+    let date
+    try {
+      date = new Date(`${this.input.value} 00:00:00`)
+    } catch (e) {}
+
+    return date && date.getTime()
+      ? date.toISOString()
       : undefined
   }
 
-  set value(value) {
+  set value(value = '') {
+    value = value instanceof Date
+      ? value.toISOString().substring(0, 10)
+      : typeof value === 'string' && value.match(/^\d{4}-\d{2}-\d{2}$/)
+        ? new Date(value).toISOString().substring(0, 10)
+        : ''
+
+    const supportsInputDate = this.input.type === 'date'
+
+    if (!supportsInputDate) {
+      const date = new Date(`${value} 00:00:00`)
+      function leadingZero(num) {
+        const s = '00' + num
+        return s.substr(s.length-2)
+      }
+
+      value = date.getTime()
+        ? `${leadingZero(date.getDate())}/${leadingZero(date.getMonth() + 1)}/${date.getFullYear()}`
+        : ''
+    }
+
     this.input.value = value
     this.input.dispatchEvent(new Event('change'))
   }
