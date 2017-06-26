ef1b802103f19f57611c97cbddb580bf2ce853ae
Darlan Mendonça <darlanmendonca@gmail.com>
2017-06-26 18:32:13 -0300

    back with scripts in app.js

--- docs/sources/app.js
+++ docs/sources/app.js
@@ -1,1 +1,43 @@
 const {input, password, number, actionSheet, form} = require('minimalist')
+
+// const angular = require('angular')
+
+// require('../../angular.js')
+// angular
+//   .module('app', ['minimalist'])
+//   .controller('HomeController', HomeController)
+
+
+// function HomeController() {
+//   this.username = 'darlanmendonca'
+//   this.password = '123123123'
+//   this.date = new Date() //'1990-09-04T03:00:00.000Z'
+//   this.number = 10
+//   this.precision = 10
+//   this.currency = 10
+//   this.percentage = 1
+
+//   this.options = [
+//     'Stark',
+//     'Lannister',
+//     'Targaryen',
+//     'Tyrell',
+//     'Martell',
+//   ]
+
+//   this.house = 'Stark'
+
+//   this.submit = (data) => {
+//     const form = document.querySelector('mn-form')
+//     console.log('angular submit now', form.data)
+//   }
+
+//   this.remove = () => {
+//     this.options.pop()
+//   }
+
+//   this.add = () => {
+//     const random = parseInt(Math.random() * 10000)
+//     this.options.push(`option ${random}`)
+//   }
+// }
