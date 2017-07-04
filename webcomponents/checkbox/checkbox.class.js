79843a36f047db31fc5796d25d57d154108fa5c4
Darlan Mendonça <darlanmendonca@gmail.com>
2017-07-04 14:22:23 -0300

    adjustments to allow navigate from keyboard

--- webcomponents/checkbox/checkbox.class.js
+++ webcomponents/checkbox/checkbox.class.js
@@ -49,6 +49,26 @@ module.exports = class MnCheckbox extends HTMLElement {
   _setCustomInput() {
     const input = document.createElement('div')
     input.classList.add('input')
+
+    const vector = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    vector.innerHTML = `
+      <g
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+        stroke-linecap="round"
+        stroke-linejoin="round">
+          <g
+            x="50%" y="50%"
+            transform="translate(5, 5)"
+            stroke-width="2">
+              <polyline
+                points="12.3825 0.581533333 3.653 10.3935333 0.273722222 6.7386"></polyline>
+          </g>
+      </g>
+    `
+    input.appendChild(vector)
     this.label.appendChild(input)
   }
 
