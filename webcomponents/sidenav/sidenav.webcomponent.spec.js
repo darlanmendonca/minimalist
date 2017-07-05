328e2cac7ec829e6082b3cc8ab9922b8a4e0ce06
alencar <lucasalencarlino@hotmail.com>
2017-07-05 10:44:42 -0300

    specs of style to mn-sidenav

--- webcomponents/sidenav/sidenav.webcomponent.spec.js
+++ webcomponents/sidenav/sidenav.webcomponent.spec.js
@@ -1,6 +1,7 @@
 /* global describe, it, before, beforeEach */
 const {expect} = require('chai')
   .use(require('chai-dom'))
+  .use(require('chai-style'))
 
 let component
 
@@ -30,6 +31,52 @@ describe('mn-sidenav (webcomponent)', () => {
 
     it('should have the .mn-card class', () => {
       expect(component).to.have.class('mn-card')
+    })
+
+    it('should have a backdrop into body', () => {
+      expect(document.body).to.have.class('mn-backdrop')
+    })
+  })
+
+  describe('css style', () => {
+    it('should have a width', () => {
+      expect(component).to.have.style('width', '320px')
+    })
+
+    it('should have a height', () => {
+      expect(component).to.have.style('height', '100vh')
+    })
+
+    it('should have a position', () => {
+      expect(component).to.have.style('position', 'fixed')
+    })
+
+    it('should have a top', () => {
+      expect(component).to.have.style('top', '0')
+    })
+
+    it.skip('should have a z-index', () => {
+      expect(component).to.have.style('z-index', '20')
+    })
+
+    it.skip('should have a transform', () => {
+      expect(component).to.have.style('transform', 'translate3d(-100%, 0, 0)')
+    })
+
+    it('should have a will-change', () => {
+      expect(component).to.have.style('will-change', 'transform')
+    })
+
+    it('should have a transition', () => {
+      expect(component).to.have.style('transition', 'transform .32s ease')
+    })
+
+    it('should have a border-radius', () => {
+      expect(component).to.have.style('border-radius', '0 !important')
+    })
+
+    it('should have a overflow', () => {
+      expect(component).to.have.style('overflow', 'auto')
     })
   })
 })
