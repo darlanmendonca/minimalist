e5cff3bbb5a77b639117f43918921a19a87a7a75
Darlan Mendonça <darlanmendonca@gmail.com>
2017-07-18 17:33:58 -0300

    use angular bind instead addEventListener

--- webcomponents/form/form.directive.js
+++ webcomponents/form/form.directive.js
@@ -8,9 +8,7 @@ function MnFormDirective() {
   return {
     restrict: 'C',
     link(scope, element, attributes) {
-      const form = element[0]
-
-      form.addEventListener('submit', () => {
+      element.bind('submit', () => {
         if (form.validate()) {
           scope.$eval(attributes.submit)
         }
