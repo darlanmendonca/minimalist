aca52d52ba91ae180b2e248efba04da0aa72b8df
Darlan Mendonça <darlanmendonca@gmail.com>
2017-07-25 17:28:10 -0300

    improve mn-autocomplete fetch()

--- webcomponents/autocomplete/autocomplete.class.js
+++ webcomponents/autocomplete/autocomplete.class.js
@@ -26,29 +26,24 @@ module.exports = class MnPassword extends MnSelect {
     super._setInput()
 
     this.input.addEventListener('input', () => {
-      this.classList.add('loading')
+      const options = this.querySelectorAll('option')
+      Array
+        .from(options)
+        .forEach(option => this.removeChild(option))
+
       const event = new Event('search')
       event.query = this.input.value
-      this.removeOptions()
       this.dispatchEvent(event)
     })
   }
 
-  createOption(text, value) {
-    const option = document.createElement('option')
-    option.textContent = text
+  fetch(request) {
+    this.classList.add('loading')
 
-    if (value !== undefined) {
-      option.setAttribute('value', value)
-    }
-
-    this.appendChild(option)
-  }
-
-  removeOptions() {
-    const options = this.querySelectorAll('option')
-    Array
-      .from(options)
-      .forEach(option => this.removeChild(option))
+    return fetch(request)
+      .then(res => {
+        this.classList.remove('loading')
+        return res.json()
+      })
   }
 }
