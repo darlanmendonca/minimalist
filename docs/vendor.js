d2d10ce6a2358b4b0fa42049763ab54cb23861f2
Darlan Mendonça <darlanmendonca@gmail.com>
2017-08-22 15:12:42 -0300

    remove last log

--- docs/vendor.js
+++ docs/vendor.js
@@ -813,7 +813,6 @@ module.exports = class MnSelect extends MnInput {
             || addedNode.textContent === this.getAttribute('value')
 
           if (isOptionSelected && !this.classList.contains('focus')) {
-            console.log(this.getAttribute('placeholder'), addedNode, addedNode.textContent)
             this.input.value = addedNode.textContent
             this.classList.add('has-value')
           }
