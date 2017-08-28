17b133523be21c415203401fe0cfccd41724f58d
Darlan Mendonça <darlanmendonca@gmail.com>
2017-08-28 15:29:32 -0300

    reenable validate on each input (mn-form validate method)

--- webcomponents/form/form.class.js
+++ webcomponents/form/form.class.js
@@ -72,9 +72,9 @@ module.exports = class MnForm extends HTMLElement {
 
   validate() {
     this.dispatchEvent(new Event('validate'))
-    // this.inputs
-    //   .filter(input => !input.hasAttribute('disabled') && !input.hasAttribute('readonly'))
-    //   .forEach(input => input.validate())
+    this.inputs
+      .filter(input => !input.hasAttribute('disabled') && !input.hasAttribute('readonly'))
+      .forEach(input => input.validate())
 
     // const isInvalid = !this.inputs.some(input => input.classList.contains('invalid'))
     const isInvalid = !this.querySelector('.invalid')
