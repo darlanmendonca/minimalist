d42756a252bb96e35d6490c340cf5642c9bb7eb1
Darlan Mendonça <darlanmendonca@gmail.com>
2017-10-16 09:32:46 -0200

    add min-height to .mn-input

--- hotsite/app.js
+++ hotsite/app.js
@@ -10,21 +10,8 @@ angular
   .controller('HomeController', HomeController)
 
 function HomeController() {
-  this.houses = ['stark', 'lannister', 'targaryen']
-  this.options = ['targaryen']
-  this.accept = true
   this.name = 'darlan'
-  // this.data = {
-  //   test: 'lero',
-  // }
-
-  this.change = () => {
-    // console.log('>', !this.accept)
-    // this.accept = !this.accept
-    this.data.test = 'lero2'
-  }
-
-  this.submit = () => {
-    console.log('submit')
-  }
+  this.houses = ['stark', 'lannister', 'targaryen']
+  this.number = 10
+  this.numbers = [10, 20, 30]
 }
