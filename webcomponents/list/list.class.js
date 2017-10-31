956d00d1b29db802ce2c1d331851d9759ec3bc7a
Darlan Mendonça <darlanmendonca@gmail.com>
2017-10-31 19:07:13 -0200

    toggle .mn-item[collapse]

--- webcomponents/list/list.class.js
+++ webcomponents/list/list.class.js
@@ -8,9 +8,24 @@ module.exports = class MnList extends HTMLElement {
 
   connectedCallback() {
     this.setStyle()
+    this.setCollapse()
   }
 
   setStyle() {
     this.classList.add('mn-list')
+  }
+
+  setCollapse() {
+    document.addEventListener('click', event => {
+      const isItemCollapse = event.target.matches(`.mn-item[collapse]`)
+      const nestedList = event.target.parentNode !== this
+
+      if (isItemCollapse && !nestedList) {
+        event.target.classList.contains('detail-visible')
+          ? event.target.classList.remove('detail-visible')
+          : event.target.classList.add('detail-visible')
+        event.stopPropagation()
+      }
+    })
   }
 }
