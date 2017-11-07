be9a50ac4925afd64f705b2dd1fdd9c687463267
Darlan Mendonça <darlanmendonca@gmail.com>
2017-11-07 17:22:08 -0200

    fix directive to elements withtou dragabble

--- docs/vendor.js
+++ docs/vendor.js
@@ -3948,12 +3948,14 @@ function MnListDirective($parse) {
     link(scope, element) {
       let model
       element.ready(() => {
-        const item = element[0].querySelector('.mn-item[ng-repeat]')
-        const expressionModel = item
-          .getAttribute('ng-repeat')
-          .match(/\sin\s([\w|\d|\.]+)/)[1]
+        const item = element[0].querySelector('.mn-item[ng-repeat][draggable]')
+        if (item) {
+          const expressionModel = item
+            .getAttribute('ng-repeat')
+            .match(/\sin\s([\w|\d|\.]+)/)[1]
 
-        model = $parse(expressionModel)(scope)
+          model = $parse(expressionModel)(scope)
+        }
       })
 
       element.on('moveItem', (event) => {
