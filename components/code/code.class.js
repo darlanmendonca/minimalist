c54be6f30798e94fca2a1871155dc4f24ba63376
Darlan Mendonça <darlanmendonca@gmail.com>
2018-01-10 21:50:30 -0200

    draft to mn-code

--- components/code/code.class.js
+++ components/code/code.class.js
@@ -6,9 +6,23 @@ module.exports = class MnCode extends HTMLElement {
 
   connectedCallback() {
     this.setStyle()
+    this.setMarkup()
   }
 
   setStyle() {
     this.classList.add('mn-code')
+  }
+
+  setMarkup() {
+    const pre = document.createElement('pre')
+    const code = document.createElement('code')
+    code.classList = this.classList
+    code.textContent = this.textContent
+      .replace('&lt;', '<')
+      .replace('&gt;', '>')
+
+    pre.appendChild(code)
+    this.textContent = ''
+    this.insertBefore(pre, this.firstChild)
   }
 }
--- docs/vendor.js
+++ docs/vendor.js
@@ -1590,10 +1590,24 @@ module.exports = class MnCode extends HTMLElement {
 
   connectedCallback() {
     this.setStyle()
+    this.setMarkup()
   }
 
   setStyle() {
     this.classList.add('mn-code')
+  }
+
+  setMarkup() {
+    const pre = document.createElement('pre')
+    const code = document.createElement('code')
+    code.classList = this.classList
+    code.textContent = this.textContent
+      .replace('&lt;', '<')
+      .replace('&gt;', '>')
+
+    pre.appendChild(code)
+    this.textContent = ''
+    this.insertBefore(pre, this.firstChild)
   }
 }
 
