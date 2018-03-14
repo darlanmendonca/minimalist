656eb83bf7f69525743b89feb11188ba8bd3c8ce
Darlan Mendonca <widarlam.t0651.imaginebeyond@xpi.com.br>
2018-03-14 14:54:22 +0000

    create mn-number doc

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const Uglify = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: {
     app: './hotsite/app.js',
     index: './app.js',
