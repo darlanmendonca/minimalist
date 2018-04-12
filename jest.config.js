135d6a75a03e1ed2dab887763d0a68a1178b636b
Darlan Mendonça <darlanmendonca@gmail.com>
2018-04-11 21:45:33 -0300

    fix preprocessor .scss files in jest

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
     'src/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    '^.+\\.(js|jsx|html|scss)$': '<rootDir>/bin/preprocessor.js',
+    '^.+\\.(js|jsx)$': '<rootDir>/bin/preprocessor.js',
+    '.scss': '<rootDir>/node_modules/jest-css-modules',
   },
   testMatch: ['<rootDir>/src/**/?(*.)(spec|test).(js|jsx)'],
   testPathIgnorePatterns: ['<rootDir>/(node_modules|bin|build)'],
