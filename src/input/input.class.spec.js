63c66365a930b975424a4843c027879725c69d65
Darlan Mendonca <widarlam.t0651.imaginebeyond@xpi.com.br>
2018-04-12 22:52:27 +0000

    add spec to focus/blur input

--- src/input/input.class.spec.js
+++ src/input/input.class.spec.js
@@ -148,6 +148,15 @@ describe('mn-input (webcomponent)', () => {
     input.setAttribute('autofocus', 'true')
     expect(input.autofocus).to.be.equal('true')
   })
+
+  test('should add/remove class focus while focus/blur element', () => {
+    expect(input).to.not.have.class('focus')
+    input.inputChild.focus()
+    expect(input).to.have.class('focus')
+
+    input.inputChild.blur()
+    expect(input).to.not.have.class('focus')
+  })
 })
 
 function createElement() {
