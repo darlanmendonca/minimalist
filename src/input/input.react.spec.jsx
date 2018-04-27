bb978cd59214a6abb284b45089a29bfccfab77c7
Darlan Mendonca <widarlam.t0651.imaginebeyond@xpi.com.br>
2018-04-27 15:57:31 +0000

    add shallow specs to react component, testing attributes

--- src/input/input.react.spec.jsx
+++ src/input/input.react.spec.jsx
@@ -1,18 +1,73 @@
-import {expect} from 'chai'
+import chai, {expect} from 'chai'
+import chaiEnzyme from 'chai-enzyme'
 import MnInput from './input.react.jsx'
 import React, {Component} from 'react'
+import Enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-let input
+chai.use(chaiEnzyme())
+Enzyme.configure({adapter: new Adapter()})
+
+let component
 
 describe('MnInput (React)', () => {
-  beforeEach(createElement)
+  beforeEach(createComponent)
 
   test('should export a class', () => {
     expect(MnInput).to.be.an('function')
   })
+
+  test('should set attribute label', () => {
+    component.setProps({label: 'lorem'})
+    expect(component).to.have.attr('label', 'lorem')
+  })
+
+  test('should set attribute value', () => {
+    component.setProps({value: 'lorem'})
+    expect(component).to.have.attr('value', 'lorem')
+  })
+
+  test('should set attribute name', () => {
+    component.setProps({name: 'lorem'})
+    expect(component).to.have.attr('name', 'lorem')
+  })
+
+  test('should set attribute placeholder', () => {
+    component.setProps({placeholder: 'lorem'})
+    expect(component).to.have.attr('placeholder', 'lorem')
+  })
+
+  test.skip('should set attribute disabled', () => {
+    component.setProps({disabled: true})
+    expect(component).to.have.attr('disabled', 'true')
+    component.setProps({disabled: false})
+    expect(component).to.not.have.attr('disabled', 'false')
+  })
+
+  test.skip('should set attribute readonly', () => {
+    component.setProps({readonly: true})
+    expect(component).to.have.attr('readonly', 'true')
+    component.setProps({readonly: false})
+    expect(component).to.not.have.attr('readonly', 'false')
+  })
+
+  test('should set attribute maxlength', () => {
+    component.setProps({maxlength: '10'})
+    expect(component).to.have.attr('maxlength', '10')
+  })
+
+  test.skip('should set attribute autocapitalize', () => {
+    component.setProps({autocapitalize: true})
+    expect(component).to.have.attr('autocapitalize', 'true')
+    component.setProps({autocapitalize: false})
+    expect(component).to.not.have.attr('autocapitalize', 'false')
+  })
 })
 
-function createElement() {
-  input = <MnInput />
+function createComponent() {
+  const app = document.createElement('div')
+  app.id = 'app'
+  document.body.appendChild(app)
+  component = shallow(<MnInput />, {attachTo: window.app})
 }
 
