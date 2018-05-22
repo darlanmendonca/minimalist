844339f6060a56e3a77a24a0cce6183be52d394b
Darlan Mendonça <darlanmendonca@gmail.com>
2018-05-22 20:19:51 -0300

    remove angular storybook and dependencies

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import {configure, setAddon} from '@storybook/react'
 import JSXAddon from 'storybook-addon-jsx'
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../../src', true, /.stories.jsx$/)
+const req = require.context('../src', true, /.stories.jsx$/)
 setAddon(JSXAddon)
 configure(loadStories, module)
 
