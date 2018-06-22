a2c7e4ef702a5c8ca6c9a5c32a4da50130c20bb0
Darlan Mendonça <darlanmendonca@gmail.com>
2018-06-22 09:56:59 -0300

    add disabled and readonly to form.story

--- src/form/form.story.jsx
+++ src/form/form.story.jsx
@@ -3,13 +3,13 @@ import {storiesOf} from '@storybook/react'
 import MnForm from './form.react.jsx'
 import MnInput from '../input/input.react.jsx'
 import MnNumber from '../number/number.react.jsx'
-import {withKnobs, text, number, boolean, select} from '@storybook/addon-knobs'
+import {withKnobs, boolean} from '@storybook/addon-knobs'
 
 storiesOf('components', module)
   .addDecorator(withKnobs)
   .addWithJSX('form', () => {
     return (
-      <MnForm>
+      <MnForm disabled={boolean('disabled', false)} readonly={boolean('readonly', false)}>
         <MnInput label='username' name='username' />
         <MnNumber label='luck number' name='luckNumber' />
       </MnForm>
