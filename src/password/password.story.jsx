c9eb5280d5bd9079e8d2afee612f098bb21bf47f
Darlan Mendonca <darlanmendonca@gmail.com>
2018-07-30 14:44:20 -0300

    fix readme from number and password

--- src/password/password.story.jsx
+++ src/password/password.story.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import MnPassword from './password.react.jsx'
 import {withKnobs, text, number, boolean, select} from '@storybook/addon-knobs'
+import {withReadme}  from 'storybook-readme'
+import readme from './README.md'
 
 storiesOf('components', module)
   .addDecorator(withKnobs)
+  .addDecorator(withReadme(readme))
   .addWithJSX('password', () => {
     return (
       <MnPassword
