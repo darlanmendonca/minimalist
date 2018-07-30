eca0f1ac287d57d0d03d0f33339db1bf47b9969b
Darlan Mendonca <darlanmendonca@gmail.com>
2018-07-30 11:27:46 -0300

    comment draft stories

--- src/image/image.story.jsx
+++ src/image/image.story.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-import MnImage from './image.react.jsx'
-import {withKnobs, text, boolean} from '@storybook/addon-knobs'
+// import React from 'react';
+// import {storiesOf} from '@storybook/react';
+// import MnImage from './image.react.jsx'
+// import {withKnobs, text, boolean} from '@storybook/addon-knobs'
 
-storiesOf('components', module)
-  .addDecorator(withKnobs)
-  .addWithJSX('image', () => {
-    return (
-      <MnImage
-        src={text('src', 'https://assets.rbl.ms/2059944/980x.jpg')}
-        perspective={boolean('perspective', false)}
-      />
-    )
-  })
+// storiesOf('components', module)
+//   .addDecorator(withKnobs)
+//   .addWithJSX('image', () => {
+//     return (
+//       <MnImage
+//         src={text('src', 'https://assets.rbl.ms/2059944/980x.jpg')}
+//         perspective={boolean('perspective', false)}
+//       />
+//     )
+//   })
--- src/loading-circle/loading-circle.story.jsx
+++ src/loading-circle/loading-circle.story.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import {storiesOf} from '@storybook/react';
-import MnLoadingCircle from './loading-circle.react.jsx'
-import {withKnobs} from '@storybook/addon-knobs'
+// import React from 'react';
+// import {storiesOf} from '@storybook/react';
+// import MnLoadingCircle from './loading-circle.react.jsx'
+// import {withKnobs} from '@storybook/addon-knobs'
 
-storiesOf('components', module)
-  .addDecorator(withKnobs)
-  .addWithJSX('loading-circle', () => {
-    return (
-      <MnLoadingCircle />
-    )
-  })
+// storiesOf('components', module)
+//   .addDecorator(withKnobs)
+//   .addWithJSX('loading-circle', () => {
+//     return (
+//       <MnLoadingCircle />
+//     )
+//   })
