c930ad9d11325b9c148bae4158f2636d656af7f2
Darlan Mendonca <darlanmendonca@gmail.com>
2018-08-01 10:59:35 -0300

    add a typing animation to display frameworks supported

--- hotsite/app.js
+++ hotsite/app.js
@@ -1,2 +1,16 @@
 import '@webcomponents/custom-elements'
+import { init as typed } from 'ityped'
 import minimalist from '../index.js'
+
+typed(document.querySelector('#typed .text'), {
+  showCursor: true,
+  backDelay: 2000,
+  strings: [
+    'vanilla js',
+    'react.js',
+    'angular 1.x',
+    'angular 2.x',
+    'vue.js',
+    'ember.js',
+  ]
+})
