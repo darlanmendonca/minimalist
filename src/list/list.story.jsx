9d6e2e862ae820a4f7e8ca473d333601693c63b4
Darlan Mendonça <darlanmendonca@gmail.com>
2018-08-28 21:20:53 -0300

    enable readme in list story

--- src/list/list.story.jsx
+++ src/list/list.story.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import {storiesOf} from '@storybook/react'
 import './list.style.scss'
 import {withKnobs, array} from '@storybook/addon-knobs'
-// import {withReadme}  from 'storybook-readme'
-// import readme from './README.md'
+import {withReadme}  from 'storybook-readme'
+import readme from './README.md'
 
 storiesOf('components', module)
   .addDecorator(withKnobs)
-  // .addDecorator(withReadme(readme))
+  .addDecorator(withReadme(readme))
   .addWithJSX('list', () => {
     const items = array('items', ['stark', 'lannister', 'targaryen'])
 
