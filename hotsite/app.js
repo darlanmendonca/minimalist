8bf61f6412edba78d6e6d577ea30f0032b51b43d
Darlan Mendonça <darlanmendonca@gmail.com>
2018-11-26 21:59:14 -0200

    adjusts in typed value (hotsite)

--- hotsite/app.js
+++ hotsite/app.js
@@ -6,11 +6,10 @@ typed(document.querySelector('#typed .text'), {
   showCursor: true,
   backDelay: 2000,
   strings: [
-    'vanilla',
-    'react',
-    'angular 1.x',
-    'angular 2.x',
-    'vue',
-    'ember',
+    'Vanilla',
+    'React',
+    'Angular',
+    'Vue',
+    'Ember',
   ]
 })
