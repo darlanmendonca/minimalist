5da8bccdf6002921321d7a6e0e7a3b04f45b5a74
Darlan Mendonça <darlanmendonca@gmail.com>
2018-12-19 21:35:00 -0200

    compile sass files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,42 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const SassPlugin = require('sass-webpack-plugin')
 const Uglify = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: {
-    app: './hotsite/app.js',
-  },
+  entry: [
+    './hotsite/app.js',
+  ],
   output: {
     path: require('path').resolve(__dirname, 'docs'),
     filename: '[name].js',
   },
   stats: 'errors-only',
   plugins: [
-    new Uglify(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'hotsite/index.pug',
-      inject: false
     }),
+    new SassPlugin('./hotsite/app.scss', {
+      sourceMap: true,
+      sass: {outputStyle: 'compressed'},
+      autoprefixer: false,
+    }),
+    new Uglify(),
   ],
   module: {
     rules: [
       {
+        test: /\.pug$/,
+        loader: [
+          'html-loader',
+          'pug-html-loader',
+        ]
+      },
+      {
         test: /\.js(x)?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-      },
-      {
-        test: /\.pug$/,
-        loader: ['html-loader?attrs=false', 'pug-html-loader']
       },
     ],
   }
