d6582a715f78a9e533623e0cdc0ed2187d23cdbf
Darlan Mendonça <darlanmendonca@gmail.com>
2018-12-18 22:51:39 -0200

    remove typed and simplify home phrase

--- hotsite/app.js
+++ hotsite/app.js
@@ -1,16 +1,2 @@
 import '@webcomponents/custom-elements'
-import { init as typed } from 'ityped'
 import minimalist from '../src/index.js'
-
-typed(document.querySelector('#typed .text'), {
-  showCursor: true,
-  backDelay: 2000,
-  strings: [
-    'vanilla',
-    'react',
-    'angular 1.x',
-    'angular 2.x',
-    'vue',
-    'ember',
-  ]
-})
